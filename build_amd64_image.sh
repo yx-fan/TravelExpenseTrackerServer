64e802ef77805c5d95eb3d92d777127dd4d0ce59
Lawrence (Yuxin) Fan <48743988+yx-fan@users.noreply.github.com>
2024-06-15 22:50:34 -0400

    build clean verison 1.0.6

--- build_amd64_image.sh
+++ build_amd64_image.sh
@@ -1,3 +1,3 @@
 export DOCKER_BUILDKIT=1
 
-docker buildx build --platform linux/amd64 -t yxfanlawrence/travelexpensebackend:1.0.5 . --push
+docker buildx build --platform linux/amd64 -t yxfanlawrence/travelexpensebackend:1.0.6 . --push
